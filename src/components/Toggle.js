f999c2db7e6787071cf251b60ab7be020a088904
Ryan Montgomery <coap.ryanmontgomery@gmail.com>
2024-08-19 02:48:54 -0600

    Update Toggle.js

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -2,11 +2,18 @@ import React, { useState } from "react";
 
 function Toggle() {
   const [isOn, setIsOn] = useState(false);
+  console.log(useState())
+  console.log(isOn)
 
   function handleClick() {
     setIsOn((isOn) => !isOn);
-}
-  return <button onClick={handleClick}>{isOn ? "ON" : "OFF"}</button>;
+    console.log(isOn)
+  }
+  const color = isOn ? "green" : "firebrick" ;
+
+  return (
+    <button style={{ background: color }} onClick={handleClick}>{isOn ? "ON" : "OFF"}</button>
+  );
 }
 
 export default Toggle;
